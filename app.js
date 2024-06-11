9894b34bbe4178935b9fb42a2df3373a452422f5
Some558 <167545573+Some558@users.noreply.github.com>
2024-06-11 14:30:05 +0900

    no message

--- app.js
+++
@@ -1,6 +0,0 @@
-const $post = document.createElement("article");
-$post.setAttribute("class","post");
-$post.innerText = "お腹減ったなあ。近所にできたラーメン屋行ってみるか";
-
-const $timeline =docment.getElementById("timeline");
-$timeline.appendChild($post);
